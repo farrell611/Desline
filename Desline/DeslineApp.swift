dfd198e87bf2b09150b9ed8adec73634b3b5e596
Farrell611 <theodorus_611@outlook.com>
2022-07-28 11:34:34 +0700

    Initial Commit

---
+++ Desline/DeslineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeslineApp.swift
+//  Desline
+//
+//  Created by Theodorus Farrell on 28/07/22.
+//
+
+import SwiftUI
+
+@main
+struct DeslineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
